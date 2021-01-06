e5807e9acf3a8823ae5a1d7aa7b843d028e7991f
zhangshan <zhangshan@zhangshandeMacBook-Pro.local>
2021-01-06 17:07:17 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: '个人blog',
+    description: 'itclanCoder的网站,专注前端技术栈分享',
+    themeConfig: {
+        nav: [{
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: '前端',
+                link: '/fontend/'
+            },
+            {
+                text: '小程序',
+                link: '/wechat/'
+            },
+            {
+                text: '面试',
+                link: '/interview/'
+            },
+            {
+                text: '关于',
+                link: '/about/'
+            },
+        ]
+    }
+}
